3571d03708b4d7932530607550e59e43c14bf503
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-11-17 17:09:32 -0800

    History file updated

--- history.c
+++ history.c
@@ -10,7 +10,7 @@
 char *get_history_file(info_t *info)
 {
 	char *buf, *dir;
-	
+
 	dir = _getenv(info, "HOME=");
 	if (!dir)
 		return (NULL);
@@ -36,10 +36,10 @@ int write_history(info_t *info)
 	ssize_t fd;
 	char *filename = get_history_file(info);
 	list_t *node = NULL;
-	
+
 	if (!filename)
 		return (-1);
-	
+
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
 	free(filename);
 	if (fd == -1)
@@ -67,10 +67,10 @@ int read_history(info_t *info)
 	ssize_t fd, rdlen, fsize = 0;
 	struct stat st;
 	char *buf = NULL, *filename = get_history_file(info);
-	
+
 	if (!filename)
 		return (0);
-	
+
 	fd = open(filename, O_RDONLY);
 	free(filename);
 	if (fd == -1)
@@ -101,6 +101,49 @@ int read_history(info_t *info)
 	while (info->histcount-- >= HIST_MAX)
 		delete_node_at_index(&(info->history), 0);
 	renumber_history(info);
-	
+
 	return (info->histcount);
+}
+
+/**
+ * build_history_list - adds entry to a history linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ * @buf: buffer
+ * @linecount: the history linecount, histcount
+ *
+ * Return: Always 0
+ */
+
+int build_history_list(info_t *info, char *buf, int linecount)
+{
+	list_t *node = NULL;
+
+	if (info->history)
+		node = info->history;
+	add_node_end(&node, buf, linecount);
+
+	if (!info->history)
+		info->history = node;
+	return (0);
+}
+
+
+/**
+ * renumber_history - renumbers the history linked list after changes
+ * @info: Structure containing potential arguments. Used to maintain
+ *
+ * Return: the new histcount
+ */
+
+int renumber_history(info_t *info)
+{
+	list_t *node = info->history;
+	int i = 0;
+
+	while (node)
+	{
+		node->num = i++;
+		node = node->next;
+	}
+	return (info->histcount = i);
 }
